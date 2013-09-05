e369e52fb171e8d8e037e34013c76a80cb747a2e
DominickJones <dojones10@kingrichardschool.net>
2013-09-05 20:12:27 +0100

    Create GUI.java

---
+++ GUI.java
@@ -0,0 +1,19 @@
+/**
+* @author Dominick Jones <DJ>
+* GUI.java : 20:11PM - 05/09/13
+*/
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.awt.GridLayout;
+
+public class GUI extends Jframe {
+  public GUI(int width, length) {
+  }
+  
+  
+  /**
+  * @param args
+  */
+  public static void main(String[] args) {
+  }

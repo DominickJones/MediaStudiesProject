b0eb0ee97fb192e98970a0288d628ac79feb878a
DominickJones <dojones10@kingrichardschool.net>
2013-09-05 20:23:43 +0100

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -8,7 +8,19 @@ import javax.swing.JButton;
 import javax.awt.GridLayout;
 
 public class GUI extends Jframe {
+  JFrame frame = new JFrame();
   public GUI(int width, length) {
+    frame.setLayout(GridLayout(int width, length));
+      grid = new JButton(width, length);
+      for (int y=0; y<length; y++) {
+        for (int x=0; x<width; x++) {
+          grid[x][y] = new JButton("("+x+", "+y+")");
+            frame.add(grid[x][y]);
+        }
+      }
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
   }
   
   
